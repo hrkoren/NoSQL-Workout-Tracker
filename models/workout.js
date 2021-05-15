9029acfa58994f604985cf3c9d7a20b58ab9f567
HRush <hrkoren@gmail.com>
2021-05-15 18:37:01 -0400

    working on seeding db

--- models/workout.js
+++ models/workout.js
@@ -2,14 +2,41 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: 'Enter a workout name'
-    }
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [{
+        type: {
+            type: String,
+            trim: true,
+            required: true
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: true
+        },
+        duration: {
+            type: Number,
+            required: true
+        },
+        weight: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        weight: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        },
+    }]
 });
 
-const Workout = mongoose.model('Workout', workoutSchema);
+const Workout = mongoose.model('Workout', WorkoutSchema);
 
 module.exports = Workout;
