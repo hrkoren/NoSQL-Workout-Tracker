baded20a19ea49e856998f96dc5a54ad238e792d
HRush <hrkoren@gmail.com>
2021-05-19 13:30:45 -0400

    added html route

--- routes/api/workouts.js
+++ routes/api/workouts.js
@@ -12,16 +12,6 @@ router.get('/api/workouts', (req, res) => {
         });
 });
 
-router.get('/api/exercise', (req, res) => {
-    Workout.find()
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
-});
-
 router.post('/api/workouts', ({ body }, res) => {
     Workout.create(body)
         .then(dbWorkout => {
